b107068038c9b9917e17ebbf87eeb8099e3436d5
CultureBMo <benjaminjmorris@hotmail.com>
2015-12-21 11:18:30 +0000

    Added icon

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.folderLabel = new System.Windows.Forms.Label();
             this.folderTextBox = new System.Windows.Forms.TextBox();
             this.browseButton = new System.Windows.Forms.Button();
@@ -82,7 +83,7 @@
             this.logTextBox.Size = new System.Drawing.Size(442, 229);
             this.logTextBox.TabIndex = 4;
             // 
-            // mainForm
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -92,7 +93,8 @@
             this.Controls.Add(this.browseButton);
             this.Controls.Add(this.folderTextBox);
             this.Controls.Add(this.folderLabel);
-            this.Name = "mainForm";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
             this.Text = "Syncify";
             this.ResumeLayout(false);
             this.PerformLayout();
