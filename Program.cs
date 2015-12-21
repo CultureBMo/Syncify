8fc22e8b1299af5e80e096f325dc7b3e8719ee43
CultureBMo <benjaminjmorris@hotmail.com>
2015-12-21 11:33:07 +0000

    More StyleCop tidying

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Syncify
+﻿namespace Syncify
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
     internal static class Program
     {
         /// <summary>
