572424aafcf982493649874c9652e238e79fa248
Ben Morris <culturebmo@outlook.com>
2020-10-29 15:13:10 +0000

    Documentation warnings suppressed

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA0001:XML comment analysis is disabled due to project configuration", Justification = "Documentation not required")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:The file header is missing or not located at the top of the file.", Justification = "Documentation not required")]
